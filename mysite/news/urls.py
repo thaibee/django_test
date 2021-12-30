07af48362433ced7c235eb8f92c8fa4fb2179a9e
magadanthai <doctor.madbee@gmail.com>
2021-12-30 08:34:21 +0700

    Lesson 12

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -4,5 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('', index),
-    path('test/', test),
+
 ]
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from news.models import News
 
 
 def index(request):
-    #print(dir(request))
-    return HttpResponse("Hello world!")
+    news = News.objects.all()
+    context = {
+        'news': news,
+        'title': 'список новостей'
+    }
+    return render(request, 'news/index.html', context)
+
 
 def test(request):
     return HttpResponse('<h1>Тестовая страница</h1>')
