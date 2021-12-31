0695df17ffb08c66cb4cc2384d181662181abf2a
magadanthai <doctor.madbee@gmail.com>
2022-01-01 06:39:09 +0700

    17-2

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from news.models import News,Category
+from news.models import News, Category
 
 
 def index(request):
@@ -14,10 +14,13 @@ def index(request):
     return render(request, 'news/index.html', context)
 
 
-def get_category(request, categody_id):
-    news = News.objects.all()
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    category = Category.objects.get(pk=category_id)
+    categories = Category.objects.all()
     context = {
         'news': news,
-        'title': 'Cписок новостей'
+        'title': category,
+        'categories': categories
     }
-    return render(request, 'news/index.html', context)
+    return render(request, 'news/category.html', context)
